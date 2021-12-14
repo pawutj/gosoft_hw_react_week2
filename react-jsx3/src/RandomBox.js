b2c5420a368e745d71a8d9b896e307c84f3492d4
astralaria <pawut.j@gmail.com>
2021-12-14 19:47:19 +0700

    Hw3.2 Done

--- react-jsx3/src/RandomBox.js
+++ react-jsx3/src/RandomBox.js
@@ -1,15 +1,22 @@
+import styled from "styled-components";
+
+const styledRandomBox = styled.div`
+  width: 500;
+  height: 300;
+  background-color: ${(props) => props.color};
+  text-align: center;
+  padding-top: 100;
+  color: white;
+`;
+
+const styledH2 = styled.h2`
+  font-size: ${(props) => props.fontSize};
+`;
+
 const RandomBox = ({ color, fontSize }) => (
-  <div style={{ width: 500, height: 300, backgroundColor: color }}>
-    <div
-      style={{
-        textAlign: "center",
-        paddingTop: 100,
-        color: "white",
-      }}
-    >
-      <h2 style={{ fontSize: ` ${fontSize}px` }}> Random Box </h2>
-    </div>
-  </div>
+  <styledRandomBox color={color}>
+    <styledH2 fontSize={fontSize}> Random Box </styledH2>
+  </styledRandomBox>
 );
 
 export default RandomBox;
